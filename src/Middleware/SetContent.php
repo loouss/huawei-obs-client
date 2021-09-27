4ba302e04337855917bea6291b2a730c7540a15b
Loouss <loouss@qq.com>
2021-09-27 14:50:40 +0800

    mime

--- src/Middleware/SetContent.php
+++ src/Middleware/SetContent.php
@@ -2,6 +2,7 @@
 
 namespace Loouss\ObsClient\Middleware;
 
+use GuzzleHttp\Psr7\MimeType;
 use Psr\Http\Message\RequestInterface;
 
 class SetContent
@@ -9,11 +10,9 @@ class SetContent
     public function __invoke(callable $handler)
     {
         return function (RequestInterface $request, array $options) use ($handler) {
-            $request = $request->withHeader(
-                'Content-MD5',
-                base64_encode(md5($request->getBody()->getContents(), true))
-            )->withHeader('Date', gmdate('D, d M Y H:i:s \G\M\T'));
-
+            $request = $request->withHeader('Content-MD5', base64_encode(md5($request->getBody()->getContents(), true)))
+                ->withHeader('Date', gmdate('D, d M Y H:i:s \G\M\T'))
+                ->withHeader('Content-Type', MimeType::fromFilename($request->getUri()->getPath()));
             return $handler($request, $options);
         };
     }
